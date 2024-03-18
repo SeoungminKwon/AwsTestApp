6330d9b4fa6fb31abd4a9c6631231df5f8aeb0a9
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-03-18 09:37:59 +0900

    feat: aws 테스트 앱

---
+++ src/main/java/com/ll/awstestapp/AwsTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.ll.awstestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsTestAppApplication.class, args);
+    }
+
+}
